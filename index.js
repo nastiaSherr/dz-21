14ae7f4857d346e41529a1e5fd9c83b9b4ab48cf
nastiaSherr <91912351+nastiaSherr@users.noreply.github.com>
2022-01-07 11:53:01 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -3,3 +3,36 @@ let price = 1000;
 price = 2000;
 
 console.log('Обрано "' + name + '" ціна за штуку ' + price + ' кредитів')
+
+
+
+const str = "Привіт";
+const num = 123;
+const flag = true;
+const txt = "true";
+
+console.log(typeof str)
+console.log(typeof num)
+console.log(typeof flag)
+console.log(typeof txt)
+
+const name2 = "Настя";
+const age = 13;
+const loggedIn = true;
+const info = "Я добре танцюю";
+const num2 = 7;
+let price2 = 400;
+let quantity = 8;
+let str2 = "test";
+let isItTrue = false;
+let people = 7800;
+console.log(name2)
+console.log(age)
+console.log(loggedIn)
+console.log(info)
+console.log(num)
+console.log(price2)
+console.log(quantity)
+console.log(str2)
+console.log(isItTrue)
+console.log(people)
